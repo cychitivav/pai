b5773f985cc9a5f12998d59007aa4df9ac45da94
Cristian Chitiva <cychitivav@unal.edu.co>
2023-05-01 14:05:22 -0500

    change theme to book and add first html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Cristian Chitiva Juan Sebastian Dueñas'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
