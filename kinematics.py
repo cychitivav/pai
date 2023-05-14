5489f52ff30580803258eb01fec8e566c7a25c10
Cristian Chitiva <cychitivav@unal.edu.co>
2023-05-14 06:23:45 -0500

    add animation

--- kinematics.py
+++ kinematics.py
@@ -13,37 +13,16 @@ class Suspension2DMechanism:
         self.phi = phi
 
     def forward_kinematics(self, q: np.ndarray):
-        # Upper mechanism
-        A = np.array([0, 0])
-        B = self.l[1] * np.array([np.cos(q[0]), np.sin(q[0])])
-        E = np.array([self.w, 0])
-        D = E + self.l[1] * np.array([np.cos(q[1]), np.sin(q[1])])
-
-        # Lower mechanism
-        r = D - B
-        alpha = np.arctan2(r[1], r[0])
-
-        r_norm = np.linalg.norm(r)
-        c_gamma = (r_norm**2 - 2*self.l[2]**2) / (2*self.l[2]**2)
-        gamma = np.arctan2(np.sqrt(1 - c_gamma**2), c_gamma)
-
-        beta = np.arccos(r_norm/(2*self.l[2]))
-        q3 = alpha - beta
-
-        C = B + self.l[2] * np.array([np.cos(q3), np.sin(q3)])
-
-        return np.linalg.norm(C - [self.w/2, self.h])
-
-
+        return np.linalg.norm(self.get_points(q)[2, :] - np.array([0, self.h]))
 
     def inverse_kinematics(self, L: float):
         pass
         
     def get_points(self, q: np.ndarray):
         # Upper mechanism
-        A = np.array([0, 0])
-        B = self.l[1] * np.array([np.cos(q[0]), np.sin(q[0])])
-        E = np.array([self.w, 0])
+        A = np.array([-self.w/2, 0])
+        B = A + self.l[1] * np.array([np.cos(q[0]), np.sin(q[0])])
+        E = np.array([self.w/2, 0])
         D = E + self.l[1] * np.array([np.cos(q[1]), np.sin(q[1])])
 
         # Lower mechanism
@@ -75,22 +54,32 @@ class Suspension2DMechanism:
         points = self.get_points(q)
         points = np.vstack((points, points[0, :]))
 
-        L = self.forward_kinematics(q)
+        L = 0.125-np.linalg.norm(points[2, :] - np.array([0, self.h]))
+        K = 400*(0.453592*9.81) * 0.0254 # N/m
+        F = K * L
 
-        plt.figure()
         plt.plot(points[:, 0], points[:, 1], 'ro-')
-        plt.plot([points[2, 0], self.w/2], [points[2, 1], self.h], 'bo-')
+        plt.plot([points[0, 0], points[4, 0]], [points[0, 1], points[4, 1]], 'ro-')
+        plt.plot([points[2, 0], 0], [points[2, 1], self.h], 'bo-')  
         plt.axis('equal')
-        plt.title(str(L) + ' m')
+        plt.title(str(L) + ' m\n' + str(F) + ' N')
         plt.grid()
-        plt.show()
 
 
 
 if __name__ == "__main__":
-    m = Suspension2DMechanism(0.1, 0.02, 0.08, 0.165, 0.03)
+    m = Suspension2DMechanism(0.1, 0.02, 0.08, 0.165, 2*0.025)
 
-    m.plot(np.array([-np.pi/6, -5*np.pi/6]))
+    for q1 in np.linspace(-np.pi/4, -np.pi/2):
+        for q2 in np.linspace(-np.pi/2, -3*np.pi/4):
+
+            plt.figure(1)
+            m.plot(np.array([q1, q2]))
+            plt.xlim([-0.3, 0.3])
+            plt.ylim([-0.1, 0.1])
+            plt.pause(0.005)
+            plt.clf()
+
 
 
 
