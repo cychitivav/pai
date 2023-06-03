8a351b161ed47fc900f4134a2037ca0d184c49d8
Sebastian <jsduenass@unal.edu.co>
2023-06-03 16:02:12 -0500

    add setup

---
+++ lib/max14870-driver/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='dual_max14870_rpi',
+      version='1.0.1',
+      description=('Library for the Pololu Dual MAX14870 Motor '
+                   'Driver Kit for Raspberry Pi'),
+      url='http://www.pololu.com/product/3758',
+      py_modules=['dual_max14870_rpi'],
+      )
