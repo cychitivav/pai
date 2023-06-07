bbea679c1b348fcc1b73f7875a5a96e785b91132
Cristian Chitiva <cychitivav@unal.edu.co>
2023-06-07 16:19:39 -0500

    Fix/driver lib remote (#29)
    * remove lib from motor_driver.py
    
    * remove lib instance and add pigpiod remote
    
    * add pigpiod to Dockerfile for use outside raspi
    
    * remove lib submodule
    
    * remove gitmodule
    
    * install `pigpiod` from pip
    
    Co-authored-by: Juan Sebastian Dueñas <jsduenass@unal.edu.co>
    
    * [FEAT]: driver with no arguments
    
    - configure driver to run with no arguments.
    
    Co-authored-by: Cristian Chitiva <cychitivav@unal.edu.co>
    
    ---------
    
    Co-authored-by: Juan Sebastian Dueñas <jsduenass@unal.edu.co>

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,13 @@ ENV QT_X11_NO_MITSHM=1
 RUN apt-get update && \
     apt-get install python3-catkin-tools -y
 
-RUN source /opt/ros/noetic/setup.bash
+RUN echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+# Install pip tools 
+RUN apt-get install -y python3-pip
+
+# Install pigpio python module
+RUN pip3 install pigpio
 
 WORKDIR /root/catkin_ws/
 
--- scripts/motor_driver.py
+++ scripts/motor_driver.py
@@ -1,52 +1,99 @@
 #!/usr/bin/python
+import sys
+
+import pigpio
 import rospy
-import rosparam
+from std_msgs.msg import Bool, Float64
 
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64, Bool
 
-import numpy as np
+class Motor():
+    MAX_SPEED = 160 * (2*3.14/60)  # 160 rpm
 
-from dual_max14870_rpi import DualMotorDriver
+    def __init__(self, pwm_pin: int, dir_pin: int, pi: pigpio.pi):
+        self.pi = pi
 
-pin = rospy.get_param(rospy.get_name(), {'FAULT':23, 'PWM2':24, 'PWM1':12, 'DIR2':16, 'DIR1':20, 'EN': 21})
+        self.pwm_pin = pwm_pin
+        self.dir_pin = dir_pin
 
-class Driver(DualMotorDriver):
-    def __init__(self):
-        super().__init__(pin['PWM1'],pin['DIR1'],pin['PWM2'],pin['DIR2'],pin['FAULT'],pin['EN'])
+    def set_speed(self, speed: float):
+        if speed < 0:
+            speed = -speed
+            dir_value = 1
+        else:
+            dir_value = 0
+
+        if speed > MAX_SPEED:
+            speed = MAX_SPEED
+
+        self.pi.write(self.dir_pin, dir_value)
+        # 20 kHz PWM, duty cycle in range 0-1000000 as expected by pigpio
+        self.pi.hardware_PWM(self.pwm_pin, 20000, int(speed * 6250 / 3))
+
+
+class DualMotorDriver():
+
+    def __init__(self, host='localhost', pin_out: dict = {'FAULT': 23, 'PWM2': 24, 'PWM1': 12, 'DIR2': 16, 'DIR1': 20, 'EN': 21}):
+        rospy.init_node("Driver", anonymous=True)
+
+        # Initialize pigpio
+        self.pi = pigpio.pi(host)
+        if not self.pi.connected:
+            rospy.signal_shutdown("Pigpio not connected, run `sudo pigpiod` or specify host")
+            exit()
+
+        # Initialize motors
+        self.motorA = Motor(pin_out['PWM1'], pin_out['DIR1'], self.pi)
+        self.motorB = Motor(pin_out['PWM2'], pin_out['DIR2'], self.pi)
+
+        # Initialize fault pin
+        self.pin_FAULT = pin_out['FAULT']
+        self.pi.set_pull_up_down(self.pin_FAULT, pigpio.PUD_UP)  # make sure FAULT is pulled up
+
+        # Initialize enable pin
+        self.pin_EN = pin_out['EN']
+        self.pi.write(self.pin_EN, 0)  # enable drivers by default
 
         # Subscriber
         self.sub_front_right = rospy.Subscriber(
-            '/wheel_A_ctrl/command', Float64, self.setSpeedM1RosWrapper)
+            '/motor_A_ctrl/command', Float64, self.set_speed_M1)
         self.sub_front_left = rospy.Subscriber(
-            '/wheel_B_ctrl/command', Float64, self.setSpeedM2RosWrapper)
-        
+            '/motor_B_ctrl/command', Float64, self.set_speed_M2)
+
         # Publishers
         self.pub_fault_warning = rospy.Publisher('/fault_warning', Bool, queue_size=10)
-        rospy.loginfo("driver start")
-        rospy.loginfo("driver pinout: " +str(pin_out))
-        
 
-    def setSpeedM1RosWrapper(self, Float64_msg):
+        rospy.loginfo("Driver start")
+        rospy.loginfo("Driver pinout: " + str(pin_out))
+
+    def set_speed_M1(self, Float64_msg):
         m1_speed = Float64_msg.data
-        self.motor1.setSpeed(m1_speed)
-        rospy.logdebug("motor 1 speed:" + str(m1_speed))       
-    
-    def setSpeedM2RosWrapper(self, Float64_msg):
+        self.motorA.set_speed(m1_speed)
+        rospy.logdebug("Motor 1 speed:" + str(m1_speed))
+
+    def set_speed_M2(self, Float64_msg):
         m2_speed = Float64_msg.data
-        self.motor2.setSpeed(m2_speed) 
+        self.motorB.set_speed(m2_speed)
         rospy.logdebug("motor 2 speed:" + str(m2_speed))
 
-    def getFaultRosWrapper(self):
-        Bool_msg = Bool
-        Bool_msg.data = self.getFault()
-        rospy.logwarn("driver fault detected")
+    def get_fault(self):
+        Bool_msg = Bool(self.getFault())
+
+        rospy.logwarn("Driver fault detected")
         self.pub_fault_warning(Bool_msg)
 
-        
+    def enable(self):
+        rospy.logwarn("Motors enabled")
+        self.pi.write(self.pin_EN, 0)
+
+    def disable(self):
+        rospy.logwarn("Motors disabled")
+        self.pi.write(self.pin_EN, 1)
 
 
 if __name__ == '__main__':
-    rospy.init_node("Driver", anonymous=True)
-    Driver()
+    if len(sys.argv) > 2:
+        pins = rospy.get_param(sys.argv[2])
+        DualMotorDriver(host=sys.argv[1], pin_out=pins)
+    else: 
+        DualMotorDriver()
     rospy.spin()
