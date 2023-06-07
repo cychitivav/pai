80bcb8925ca8882231f0807f5ec16b2e7ee573ee
Cristian Chitiva <cychitivav@unal.edu.co>
2023-06-07 13:29:34 -0500

    Dev/battery sensor (#27)
    * add attiny reader
    
    * finish arduino program for attiny85
    
    * add battery node
    
    * add battery script to cmakelists
    
    * fix import
    
    * fix import std_msgs
    
    * custom service
    
    * fix srv data
    
    * fix battery constants
    
    * fix power battery names
    
    * fix serial number
    
    * refactor attiny name
    
    * fix BatteryState msg
    
    * add rospy logs
    
    * fix serialization error
    
    * change pin to PB3 in arduino file
    
    * change voltage to float 32 in srv
    
    * add documetation for bootloader attiny85
    
    * add service response
    
    * fix rate for publish and improve loginfo
    
    * remove inheritance from battery node

---
+++ scripts/battery.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+import sys
+
+import pigpio
+import rospy
+from sensor_msgs.msg import BatteryState
+
+from pai.srv import CalibrateBattery, CalibrateBatteryResponse
+
+
+class Battery():
+    def __init__(self, host='localhost'):
+        rospy.init_node('Battery', anonymous=True)
+
+        self.pi = pigpio.pi(host)
+        if not self.pi.connected and not rospy.is_shutdown():
+            rospy.signal_shutdown("Pigpio not connected, run 'sudo pigpiod'")
+            exit()
+
+        self.ATtiny85 = self.pi.i2c_open(1, 0x08)  # Open i2c bus 1, slave address 0x08 (ATtiny85)
+        self.factor = 24.6/435  # Calibration factor
+
+        self.pub = rospy.Publisher('/battery', BatteryState, queue_size=10)
+        self.ser = rospy.Service('/calibrate_battery', CalibrateBattery, self.update_calibration)
+
+        msg = BatteryState()
+
+        msg.temperature = float('nan')
+        msg.current = float('nan')
+        msg.charge = float('nan')
+        msg.capacity = float('nan')
+        msg.design_capacity = 7.8
+
+        msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+        msg.power_supply_health = BatteryState.POWER_SUPPLY_HEALTH_UNKNOWN
+        msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LION
+
+        msg.present = True
+
+        msg.cell_voltage = [float('nan')]*18
+        msg.cell_temperature = [float('nan')]*18
+
+        msg.serial_number = '05638'
+        msg.location = 'Base'
+
+        rate = rospy.Rate(1/60)  # Once per minute
+        while not rospy.is_shutdown():
+            msg.header.stamp = rospy.Time.now()
+
+            msg.voltage = self.get_voltage()
+            msg.percentage = msg.voltage/25.2
+
+            self.pub.publish(msg)
+            rate.sleep()
+
+    def get_voltage(self):
+        # Read int(2 bytes) from slave when it sends data
+        (_, data) = self.pi.i2c_read_device(self.ATtiny85, 2)
+        # Convert bytes to float
+        data = int.from_bytes(data, byteorder='big') * self.factor
+        rospy.logdebug("Battery voltage: %f", data)
+
+        return data
+
+    def update_calibration(self, req):
+        # Get current battery number
+        (_, data) = self.pi.i2c_read_device(self.ATtiny85, 2)
+        data = int.from_bytes(data, byteorder='big')
+
+        # Update calibration factor
+        if data:
+            self.factor = req.voltage / data
+
+            rospy.loginfo("Battery calibrated:\n\
+                           \tBattery voltage: %f\n\
+                           \tBattery percentage: %f", req.voltage, req.voltage/25.2)
+            rospy.logdebug("Calibration factor updated to %f", self.factor)
+        else:
+            rospy.logerr("Battery number is 0, check battery connection")
+
+        return CalibrateBatteryResponse()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        Battery(sys.argv[1])
+    else:
+        Battery()
+
+    rospy.spin()
