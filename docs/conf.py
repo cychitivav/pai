ad2559ecafed70ddc78d6fc56f429dbd3e7177ba
Cristian Chitiva <cychitivav@unal.edu.co>
2023-06-11 08:37:52 -0500

    finish latex options

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,13 @@ version = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = ['myst_nb']
+extensions = ['myst_parser']  # , 'myst_nb']
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-    'ipynb': 'myst-nb'
-}
+# source_suffix = {
+#     '.rst': 'restructuredtext',
+#     '.md': 'markdown',
+#     'ipynb': 'myst-nb'
+# }
 
 numfig = True
 
@@ -36,62 +36,64 @@ math_number_all = True
 html_theme = 'sphinx_book_theme'
 
 html_search_language = 'es'
+
 # -- Options for LaTeX output ------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
 latex_engine = 'xelatex'
 
-# latex_documents = ['main', 'informe', 'Vehículo de muestreo', 'Camilo Andrés Barrera Castellanos\\Camilo Andrés Borda Gil\\Cristian Yesid Chitiva Vela\\Johan Andrés Díaz Roa\\Juan Sebastian Dueñas Salamanca\\Juan Fernando Mendieta Hernández\\Juan Pablo Vallejo Montañez', 'howto', 'true']
+latex_documents = [(
+    'index', 
+    'main.tex', 
+    project,
+    author.replace(', ', '\\and '),
+    'howto',
+    False
+    )
+]
 
-# latex_logo = 'images/logounal.png'
+latex_logo = 'images/logounal.png'
 latex_toplevel_sectioning = 'section'
 latex_show_urls = 'footnote'
 
 latex_elements = {
-    'papersize': r'letterpaper',
-    'pointsize': r'12pt',
-    'babel': r'\usepackage[spanish,mexico]{babel}',
-    'fontpkg': r'''
-        \usepackage{fontspec}
-        \setmainfont{Ancizar Sans}[
+    'papersize': 'letterpaper',
+    'pointsize': '12pt',
+    'babel': '\\usepackage[spanish,mexico]{babel}',
+    'fontpkg': '''
+        \\usepackage{fontspec}
+        \\setmainfont{Ancizar Sans}[
             ItalicFont = * Italic, 
             BoldFont = * Bold, 
             BoldItalicFont = * Bold Italic
         ]
     ''',
-    'preamble': r'''
-        \usepackage{mathtools}
-        \usepackage{pdfpages}
-        \usepackage{amsmath}
-        \usepackage{amsfonts}
-        \usepackage{amssymb}
-        \usepackage{graphicx}
-        \usepackage{float}
-        \usepackage{pdfpages}
-        \usepackage[titles]{tocloft}
+    'preamble': '''
+        \\usepackage{mathtools}
+        \\usepackage{pdfpages}
+        \\usepackage{amsmath}
+        \\usepackage{amsfonts}
+        \\usepackage{amssymb}
+        \\usepackage{graphicx}
+        \\usepackage{float}
+        \\usepackage{pdfpages}
+        \\usepackage[titles]{tocloft}
     ''',
-    'maketitle': r'''
-        \begin{titlepage}
-            \centering
-            \includegraphics[width=0.7\textwidth]{logounal.png}     \\  \vspace{\fill}
-            \LARGE \textbf{Vehículo de muestreo}                     \\  \vspace{\fill}
-            \normalsize \textsl{Camilo Andrés Barrera Castellanos\\
-                                Camilo Andrés Borda Gil\\
-                                Cristian Yesid Chitiva Vela\\
-                                Johan Andrés Díaz Roa\\
-                                Juan Sebastian Dueñas Salamanca\\
-                                Juan Fernando Mendieta Hernández\\
-                                Juan Pablo Vallejo Montañez}        \\  \vspace{\fill}
-            \textbf{Profesor:}                                      \\
-            \textsl{Ing. Carlos Alberto Narváez Tovar}              \\   \vspace{\fill}
-            Universidad Nacional de Colombia                        \\
-            Facultad de Ingeniería                                  \\
-            Departamento de Ingeniería Mecánica y Mecatrónica       \\
-            Proyecto Aplicado de Ingeniería                         \\
+    'maketitle': '''
+        \\begin{titlepage}
+            \\centering 
+            \\includegraphics[width=0.7\\textwidth]{logounal.png}   \\\\    \\vspace{\\fill}
+            \\LARGE \\textbf{%s}                                    \\\\    \\vspace{\\fill}
+            \\normalsize \\textsl{%s}                               \\\\    \\vspace{\\fill}
+            \\textbf{Profesor:}                                     \\\\
+            \\textsl{Ing. Carlos Alberto Narváez Tovar}             \\\\    \\vspace{\\fill}
+            Universidad Nacional de Colombia                        \\\\
+            Facultad de Ingeniería                                  \\\\
+            Departamento de Ingeniería Mecánica y Mecatrónica       \\\\
+            Proyecto Aplicado de Ingeniería                         \\\\
             Bogotá, Colombia                                        
-            \the\year
+            \\the\\year
         \end{titlepage}
-    ''',
+    ''' % (project, author.replace(', ', '\\\\')),
     'figure_align': 'H',
-    'geometry': '\\usepackage[letterpaper,margin=3cm]{geometry}',
     'hyperref': '\\usepackage{hyperref}'
 }
