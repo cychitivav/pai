5f098be2d402cf1d4b313fa2c6182ae8684ab88e
Cristian Chitiva <cychitivav@unal.edu.co>
2023-06-14 15:03:17 -0500

    delete unnecessary libraries and read current state

--- scripts/motor_driver.py
+++ scripts/motor_driver.py
@@ -4,8 +4,6 @@ import sys
 import pigpio
 import rospy
 from std_msgs.msg import Bool, Float64
-import numpy as np
-from operator import xor
 
 
 class Motor():
@@ -18,17 +16,14 @@ class Motor():
         self.dir_pin = dir_pin
 
         # Set range to 0-100
-        self.pi.set_PWM_range(self.pwm_pin, 100)
-
+        self.pi.set_PWM_range(self.pwm_pin, 10000)
         # Set frequency to 8kHz
         self.pi.set_PWM_frequency(self.pwm_pin, 8000)
 
-        self.abs_speed = 0
-        self.dir_value = self.pi.read(self.dir_pin)
-
     def set_speed(self, speed: float):
-        self.dir_value = self.pi.read(self.dir_pin)
-        # abs speed
+        current_dir = self.pi.read(self.dir_pin)
+        current_speed = self.pi.get_PWM_dutycycle(self.pwm_pin)
+
         if speed < 0:
             speed = -speed
             dir_value = 1
@@ -39,37 +34,14 @@ class Motor():
             speed = self.MAX_SPEED
 
         # change of direction protection
-        change_dir = xor(dir_value, self.dir_value)
-        if change_dir and max(self.abs_speed, speed) > 0.05*self.MAX_SPEED:
+        if dir_value != current_dir and max(current_speed, speed) > 0.05*self.MAX_SPEED:
             self.pi.write(self.pwm_pin, 0)
-            rate = rospy.Rate(10)       # 10 Hz -> T = 0.1 s
-            rate.sleep()
+            rospy.sleep(0.1)  # 100ms
 
         # Set direction
         self.pi.write(self.dir_pin, dir_value)
-        self.dir_value = dir_value
-
         # Set duty cycle to speed
-        self.pi.set_PWM_dutycycle(self.pwm_pin, speed)
-        self.abs_speed = speed
-
-    def set_ramp_speed(self, target_speed: float):
-
-        rate = rospy.Rate(204)  # 120hz
-
-        if self.dir_value:
-            current_speed = -self.abs_speed
-        else:
-            current_speed = self.abs_speed
-
-        step = np.sign(target_speed-current_speed)*2*self.MAX_SPEED/100
-
-        ramp = range(current_speed, target_speed, step)
-
-        for speed in ramp:
-            self.set_speed(speed)
-            rate.sleep()
-            rospy.logwarn("Motor speed: " + str(self.speed))
+        self.pi.set_PWM_dutycycle(self.pwm_pin, speed*100)
 
 
 class DualMotorDriver():
@@ -90,15 +62,15 @@ class DualMotorDriver():
         # Initialize fault pin
         self.pin_FAULT = pin_out['FAULT']
         # make sure FAULT is pulled up
-        self.pi.set_pull_up_down(self.pin_FAULT, pigpio.PUD_UP)  
+        self.pi.set_pull_up_down(self.pin_FAULT, pigpio.PUD_UP)
 
         # Initialize enable pin
         self.pin_EN = pin_out['EN']
         # enable drivers by default
-        self.pi.write(self.pin_EN, 0)  
+        self.pi.write(self.pin_EN, 0)
         # Check for faults
         self.pi.callback(self.pin_FAULT, pigpio.FALLING_EDGE,
-                         self.get_fault)  
+                         self.get_fault)
 
         # Subscriber
         self.sub_front_right = rospy.Subscriber(
